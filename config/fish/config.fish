22d804120b7c5c659435b29ae6c80ddb915677a5
edouardparis <m@edouard.paris>
2019-09-28 14:47:08 +0200

    feat fish git

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -6,4 +6,11 @@ set -Ux EDITOR $VISUAL
 set GOPATH $HOME/go
 set GOBIN $HOME/go/bin
 set PATH $PATH $GOBIN
+set PATH $PATH $HOME/.cargo/bin
+set PATH $PATH /usr/local/go/bin
+
 source $HOME/.config/fish/fish_prompt.fish
+source $HOME/.config/fish/git.fish
+
+eval (starship init fish)
+eval (ssh-agent -c) > /dev/null
